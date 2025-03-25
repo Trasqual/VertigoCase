c99be884e3a5304051df9578e326116cdac298cc
trasqual <tolgaakad@gmail.com>
2025-03-25 23:08:41 +0300

    Update IReward.cs

--- Assets/Scripts/RewardSystem/IReward.cs
+++ Assets/Scripts/RewardSystem/IReward.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class IReward : MonoBehaviour
+namespace RewardSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    public interface IReward
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void Claim();
     }
 }
