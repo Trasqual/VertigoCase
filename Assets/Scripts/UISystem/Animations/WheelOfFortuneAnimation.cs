dcbea33619e143fa15e87b750f04ef8f82c33027
trasqual <tolgaakad@gmail.com>
2025-03-27 12:53:01 +0300

    Added wheel of fortune animation

---
+++ Assets/Scripts/UISystem/Animations/WheelOfFortuneAnimation.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+namespace UISystem.Animations
+{
+    public class WheelOfFortuneAnimation : MonoBehaviour
+    {
+        [SerializeField] private int _rewardCount = 8;
+        [SerializeField] private float _spinDuration = 2f;
+        [SerializeField] private float _minSpinSpeed = 90f;
+        [SerializeField] private float _maxSpinSpeed = 1440f;
+        [SerializeField] private float _snapSpeed = 50f;
+    
+        private bool _isSpinning;
+    
+        public void Play()
+        {
+            if (_isSpinning)
+            {
+                return;
+            }
+        
+            StartCoroutine(SpinCoroutine());
+        }
+
+        private IEnumerator SpinCoroutine()
+        {
+            _isSpinning = true;
+        
+            float elapsedTime = 0f;
+            const float maxSpeedRandomnessRange = 0.8f;
+            float speed = Random.Range(_maxSpinSpeed * maxSpeedRandomnessRange, _maxSpinSpeed);
+
+            while (elapsedTime < _spinDuration)
+            {
+                float angleIncrement = speed * Time.deltaTime;
+                transform.Rotate(Vector3.forward, angleIncrement);
+            
+                const float deceleration = 0.98f;
+                speed = Mathf.Max(_minSpinSpeed, speed * deceleration);
+            
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+        
+            float stoppedAngle = transform.eulerAngles.z;
+            float closestStopAngle = GetClosestStopAngle(stoppedAngle);
+        
+            yield return SnapToAngle(closestStopAngle);
+        
+            _isSpinning = false;
+        }
+
+        private Tween SnapToAngle(float targetAngle)
+        {
+            return transform.DORotate(new Vector3(0f,0f,targetAngle), _snapSpeed).SetSpeedBased();
+        }
+
+        private float GetClosestStopAngle(float angle)
+        {
+            float angleStep = 360f / _rewardCount;
+            float closestAngle = Mathf.Round(angle / angleStep) * angleStep;
+            return closestAngle;
+        }
+
+        public int GetClosestStopIndex()
+        {
+            float angleStep = 360f / _rewardCount;
+            int closestStop = Mathf.RoundToInt(transform.eulerAngles.z / angleStep);
+            return closestStop;
+        }
+    }
+}
